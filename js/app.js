842587992a04d064376352bb0e27e311c57c2e38
Tibor Szasz <tszasz@gmail.com>
2020-01-03 00:59:44 +0100

    feat(js): init canvas

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 /**
- * Basic anim setup
+ * Load hero shader
  */
+import GlslCanvas from 'glslCanvas';
+import shader from '../shaders/sdf.glsl';
 
+const root = document.querySelector('.hero');
+const canvas = document.createElement("canvas");
+const {width, height} = root.getBoundingClientRect();
 
-// function renderer() {
+canvas.width = width / window.devicePixelRatio;
+canvas.height = height / window.devicePixelRatio;
 
-// }
+var sandbox = new GlslCanvas(canvas);
+sandbox.load(shader);
 
-// function animate() {
-
-//     requestAnimationFrame( animate );
-//     // renderer();
-// }
-
-// animate();
+root.appendChild(canvas);
